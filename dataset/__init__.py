a2ce6652eb1706e7d2b17d482904e7de5b758101
pengbo <stevehugo922@gmail.com>
2022-06-08 22:37:02 +0800

    add mask dataset for landmark

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,11 +1,12 @@
 '''
 Author: Peng Bo
 Date: 2022-05-15 21:49:17
-LastEditTime: 2022-06-03 02:40:06
+LastEditTime: 2022-06-08 22:33:17
 Description: 
 
 '''
 from .landmarks_dataset import *
+from .landmarks_mask_6_dataset import *
 from .landmarks_6_dataset import *
 from .landmarks_mask_dataset import *
 from .dataloaderX import DataLoaderX
