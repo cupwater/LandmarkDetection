f897b14029d326891aed6ac578d91f0962f33b13
pengbo <stevehugo922@gmail.com>
2022-06-08 13:27:30 +0800

    fix augmentation bug

--- augmentation/__init__.py
+++ augmentation/__init__.py
@@ -1,8 +1,8 @@
 '''
 Author: Pengbo
 Date: 2022-03-04 23:49:49
-LastEditTime: 2022-05-15 20:59:54
+LastEditTime: 2022-06-08 10:48:03
 Description: 
 
 '''
-from .medical_augment import LmsDetectTestTransform, LmsDetectTrainTransform
+from .medical_augment import LmsDetectTestTransform, LmsDetectTrainTransform, rotate, translate
--- dataset/landmarks_dataset.py
+++ dataset/landmarks_dataset.py
@@ -5,26 +5,28 @@ Description: Landmarks Detection dataset
 
 '''
 import os
-import time
-
 import numpy as np
 import cv2
 
 import torch
 from torch.utils.data import Dataset
-import pdb
 
 from .util import gaussianHeatmap
-import matplotlib.pyplot as plt
+
+import sys
+sys.path.insert(1, '../augmentation')
+from .medical_augment import rotate, translate
 
 __all__ = ['ChestLandmarkDataset']
 
+
+
 class ChestLandmarkDataset(Dataset):
 
-    def __init__(self, img_list, meta, transform, prefix='data/', img_size=(512, 512), sigma=5):
+    def __init__(self, img_list, meta, transform_paras, prefix='data/', img_size=(512, 512), sigma=5):
 
         self.img_size = tuple(img_size)
-        self.transform = transform
+        self.transform_paras = transform_paras
         self.prefix = prefix
         # read img_list and metas
         self.img_list = [l.strip() for l in open(img_list).readlines()]
@@ -40,6 +42,7 @@ class ChestLandmarkDataset(Dataset):
         for index in range(len(self.img_list)):
             img_path = os.path.join(self.prefix, self.img_list[index].strip())
             img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+            # print(f'max: {np.max(img)}, min:{np.min(img)}')
             img = cv2.resize(img, self.img_size)
             img_data_list.append(img)
         return img_data_list
@@ -48,38 +51,41 @@ class ChestLandmarkDataset(Dataset):
         img = self.img_data_list[index]
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         w, h, _ = img.shape
-        lms_img = img.copy()
         img = img.transpose((2, 0, 1))
 
         # get the coordinates of landmark in image
         lms = self.lms_list[index]
-        # lms = [ (int(lms[i]*w), int(lms[i+1]*h)) for i in range(0, int(len(lms)), 2)]
         lms = [ (int(lms[i+1]*h), int(lms[i]*w)) for i in range(0, int(len(lms)), 2)]
 
         lms_heatmap = [self.genHeatmap(point, (w, h)) for point in lms]
         lms_heatmap = np.array(lms_heatmap)
 
-        ### 将 landmarks 标记到图像 pic 上，并输出每个 landmark 的 heatmap
-        if not os.path.exists('./runs/gaussianHeatmap'):
-            os.makedirs('./runs/gaussianHeatmap')
-        exe = str(time.time())[-6:]
+        def trans(*imgs, transform_list=None):
+            ''' img: chanel x imageshape
+            '''
+            ret = []
+            for img in imgs:
+                # copy is necessary, to avoid modifying origin data
+                cur_img = img.copy()
+                for f in transform_list:
+                    cur_img = f(cur_img)
+                    # print(f'max: {np.max(cur_img)}, min:{np.min(cur_img)}')
+                # copy is necessary, torch needs ascontiguousarray
+                ret.append(cur_img.copy())
+            return tuple(ret)
 
-        from dataset.util import getPointsFromHeatmap
-        points = getPointsFromHeatmap(lms_heatmap)
-        # for i,num,lm in zip(lms_heatmap, range(int(len(lms_heatmap))), lms):
-        #     # 生成的 lms_heatmap 中，坐标x，y对换了
-        #     plt.imsave('./runs/gaussianHeatmap/{}_{}_{}_{}.png'.format(str(exe),num,lm[0],lm[1]), i)
-
-        # for p in points:
-        #     # 回归出的坐标是正确的
-        #     cv2.circle(lms_img, (int(p[1]),int(p[0])), 2, (0,255,0), 2)
-        # plt.imsave('./runs/gaussianHeatmap/{}.png'.format(str(exe)), lms_img)
-        ###
-
-        img, lms_heatmap = self.transform(img, lms_heatmap)
+        transform_list = []
+        # transform
+        if np.random.rand() < 0.5:
+            transform_list.append(rotate((np.random.rand()-0.5)*self.transform_paras['rotate_angle']))
+        if np.random.rand() < 0.5:
+            rorate_x = int((np.random.rand()-0.5)*self.transform_paras['offset'][0])
+            rorate_y = int((np.random.rand()-0.5)*self.transform_paras['offset'][1])
+            transform_list.append(translate([rorate_x, rorate_y]))
+    
+        img, lms_heatmap = trans(img, lms_heatmap, transform_list=transform_list)
         img, lms_heatmap = torch.FloatTensor(img), torch.FloatTensor(lms_heatmap)
 
-        # img 正，lms_heatmap 正
         return img, lms_heatmap
 
     def __len__(self):
@@ -87,32 +93,29 @@ class ChestLandmarkDataset(Dataset):
 
 
 if __name__ == "__main__":
-    prefix = 'data/26_landmarks'
-    img_list = 'data/imglist_filter_train.txt'
-    meta = 'data/lms_filter_train.txt'
+    prefix = '../data/26_landmarks'
+    img_list = '../data/imglist_filter_train.txt'
+    meta = '../data/lms_filter_train.txt'
 
-    import sys
-    sys.path.insert(1, 'augmentation')
-    from medical_augment import LmsDetectTrainTransform
-    transform = LmsDetectTrainTransform()
-    chest_dataset = ChestLandmarkDataset(img_list, meta, transform, prefix)
-
+    transform_list = {'rotate_angle': 1, 'offset': [1,1]}
+    chest_dataset = ChestLandmarkDataset(img_list, meta, transform_list, prefix)
 
     for i in range(chest_dataset.__len__()):
-        print(i)
         image, lms_heatmap = chest_dataset.__getitem__(i)
         image, lms_heatmap = image.numpy(), lms_heatmap.numpy()
-
-        image = np.transpose(image, (1,2,0))
+        print(f'max: {np.max(image)}, min:{np.min(image)}')
+        image = np.transpose(image, (1,2,0)) * 255
         image = image.astype(np.uint8)
 
         lms_heatmap = np.sum(lms_heatmap, axis=0) * 255
         lms_heatmap[lms_heatmap>255] = 255
         lms_heatmap = lms_heatmap.astype(np.uint8)
-
-        # pdb.set_trace()
         cv2.imshow('heatmap', lms_heatmap)
+        
         cv2.imshow('image', image)
+        
         key=cv2.waitKey(-1)
         if key != 27:
             continue
+        else:
+            exit(0)
